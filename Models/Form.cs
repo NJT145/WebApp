031b6501a51dd53f517f807465605dbafc5e1fb9
Nejat Günaydın <njt145@gmail.com>
2019-10-13 13:09:57 +0300

    ...

--- Models/Form.cs
+++ Models/Form.cs
@@ -36,24 +36,38 @@ namespace WebApp.Models
 
         public bool ForkliftLicence { get; set; }
         public bool CraneOperationLicense { get; set; }
+
         [ForeignKey("CourseNSeminar")]
         public int CourseNSeminarId { get; set; }
+
         public virtual CourseNSeminar CourseNSeminar { get; set; }
+
         [ForeignKey("LangInfo")]
         public int LangInfoId { get; set; }
+
         public virtual LangInfo LangInfo { get; set; }
+
         [ForeignKey("ComputerSkill")]
         public int ComputerSkillId { get; set; }
+
         public virtual ComputerSkill ComputerSkill { get; set; }
+
         // sağlık durumu ## 4.SAYFA ## ekle
         public bool WorkingNow { get; set; }
+
         public string JobChangeReason { get; set; }
+
         [ForeignKey("JobHistory")]
         public int JobHistoryId { get; set; }
+
         public virtual JobHistory JobHistory { get; set; }
         public bool Ok4Overtime { get; set; }
         public bool Ok4ShiftWork { get; set; }
-        // 5.SAYFA gerisi eklenecek
+
+        [ForeignKey("FormResult")]
+        public int FormResultId { get; set; }
+
+        public virtual FormResult FormResult { get; set; }
     }
 
     public class FormDbContext : DbContext
---
+++ Models/FormResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class FormResult
+    {
+        [Key]
+        public int FormResultId { get; set; }
+        [ForeignKey("Form")]
+        public int FormId { get; set; }
+        public bool Confirmed { get; set; }
+        public string Price { get; set; }
+        public string ShiftWork { get; set; }
+        public string ShiftWork2 { get; set; }
+        public string MilitaryService { get; set; }
+        public string EduStatus { get; set; }
+        public string Address { get; set; }
+        public virtual ICollection<Form> Forms { get; set; }
+    }
+}
